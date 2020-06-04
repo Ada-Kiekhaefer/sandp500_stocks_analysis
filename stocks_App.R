d835f7547d561ab61e6977916da4f0eb24353f78
Ada-Kiekhaefer <ada.kiekhaefer@gmail.com>
2020-06-04 10:00:43 -0500

    update stock_App

--- stocks_App.R
+++ stocks_App.R
@@ -20,9 +20,14 @@ server <- function(input, output){
     paste(toupper(input$stock_symbol), 'stock time-series plot')
   })
  output$trend <- renderPlot({
-   ggplot(subset(all_stocks, Name == toupper(input$stock_symbol))) +
-     geom_line(aes(x = date, y = close)) +
-     labs(y = 'stock price (dollars)')
+   # ggplot(subset(all_stocks, Name == toupper(input$stock_symbol))) +
+   #   geom_line(aes(x = date, y = close)) +
+   #   labs(y = 'stock price (dollars)')
+   s_name <- all_stocks %>% 
+     filter(Name == toupper(input$stock_symbol))
+   ggplot(s_name) +
+   geom_line(aes(x = date, y = close)) +
+   labs(y = 'stock price (dollars)')
  }) 
  output$volume <- renderPlot({
    ggplot(subset(all_stocks, Name == toupper(input$stock_symbol))) +
